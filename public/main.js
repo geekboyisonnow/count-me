a23ba7bd880f01d719712e8922e799f4f19c075e
Kenneth Cannon <kennethfcannon@gmail.com>
2019-09-21 20:54:17 -0400

    Created a simple Counter App

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
+let count = 0
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  // write all my code here inside main
+
+  // Find the p tag
+  const ptag = document.querySelector('p')
+  console.log(ptag)
+
+  // Change the p tag text to the variable count.
+  ptag.textContent = count
+
+  // When the user clicks the button; 
+  // find the button
+
+  const button = document.querySelector('button')
+  console.log(button)
+
+  // When the button is clicked - call a Function
+  // increment the count 
+
+
+  // Change the p tag to show the variable count
+
+
+  // if (document.querySelector('h1.hello-world')) {
+  //   document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  // }
 }
 
 document.addEventListener('DOMContentLoaded', main)
